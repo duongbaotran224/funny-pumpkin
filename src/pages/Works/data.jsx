4c73f2a561e4737f2354c2775ee9047ceec6bf43
duong bao tran <bao.tran@m1studio.co>
2020-04-02 11:13:25 +0700

    clean page-works

--- src/pages/Works/data.jsx
+++ src/pages/Works/data.jsx
@@ -1,24 +1,28 @@
 // import uuid from 'uuid/v1';
 // import moment from 'moment';
 
-export default [
-  {
-    "title": "Menu Animation",
-    "image": "",
-    "date": "2019"
-  },
-  {
-    "title": "Menu Animation",
-    "image": "",
-    "date": "2019"
-  },
-  {
-    "title": "Menu Animation",
-    "image": "",
-    "date": "2019"
-  }, {
-    "title": "Menu Animation",
-    "image": "",
-    "date": "2019"
-  },
-];
+export default {
+  title: "WORKS",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod",
+  list: [
+    {
+      "title": "Menu Animation",
+      "image": "",
+      "date": "2019"
+    },
+    {
+      "title": "Menu Animation",
+      "image": "",
+      "date": "2019"
+    },
+    {
+      "title": "Menu Animation",
+      "image": "",
+      "date": "2019"
+    }, {
+      "title": "Menu Animation",
+      "image": "",
+      "date": "2019"
+    },
+  ]
+}
